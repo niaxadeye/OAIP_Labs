41738ae05ab6b8258d37f22ec04eaef74dd4ac03
Эрик Фаттахов <niaxadeye@outlook.com>
2020-09-18 13:34:50 +0300

    Загрузил 1 задание 1 лаборатной работы

---
+++ Lab1/Lab1_1/Lab1_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>                                 
+#include <math.h>                                       
+float alg(int x, int y, int  z) {
+    float ans;
+    ans = (pow(3, x) - (y - 2) * (y - 2)) / z;
+    return (ans);
+
+}
+int main() {
+    int x, y, z;
+    float ans;
+    scanf_s("%d%d%d", &x, &y, &z);
+    ans = alg(x, y, z);
+    printf("%f", ans);
+    return 0;
+}
