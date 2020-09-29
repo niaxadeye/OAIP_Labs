e566f925e42df284b95dccd9fc1b8fc26df8e433
Эрик Фаттахов <niaxadeye@outlook.com>
2020-09-29 09:45:20 +0300

    финал

--- Lab1/Lab1_2/Lab1_2.cpp
+++ Lab1/Lab1_2/Lab1_2.cpp
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>                                 
 #include <math.h>     
 #include <locale.h>   
-float alg(double x, double y, double  z, double t) {
+double alg(int x, int y, int  z, int t) {
     double ans;
     ans = ((x + pow((y + 1), 3))) / ((z - 4)*(pow(t,2) - 4));
     return (ans);
@@ -9,27 +9,38 @@ float alg(double x, double y, double  z, double t) {
 }
 int main() {
     setlocale(LC_ALL, "Rus");
-    double ans, x, y, z, t;
-    printf_s("Выражение ((x + (y + 1)^3)) / ((z - 11)*(t^2 - 4)) : \n");
+    int x, y, z, t;
+    double ans;
+    printf_s("Выражение ((x + (y + 1)^3)) / ((z - 4)*(t^2 - 4)) : \n");
     printf_s("Введите переменную x: ");
-    scanf_s("%lf", &x);
+    scanf_s("%d", &x);
     printf_s("Введите переменную y: ");
-    scanf_s("%lf", &y);
+    scanf_s("%d", &y);
     printf_s("Введите переменную z: ");
-    scanf_s("%lf", &z);
+    scanf_s("%d", &z);
     printf_s("Введите переменную t: ");
-    scanf_s("%lf", &t);
-    if (z == 11 || t == -2 || t == 2) {
-        if (z == 11) {
-            printf_s("z не может равняться 4, т.к. деление на 0 недопустимо \n");
-        }
+    scanf_s("%d", &t);
+    if (z == 11 &&( t == -2 || t == 2)) {
+        printf_s("z не может равняться 4, т.к. деление на 0 недопустимо \n");
         if (t == -2) {
-            printf_s("t не может равняться -2, т.к. деление на 0 недопустимо \n");
+            printf_s("t не может равняться -2 , т.к. деление на 0 недопустимо \n");
         }
-        if (t == 2) {
-            printf_s("z не может равняться 2, т.к. деление на 0 недопустимо \n");
+        else {
+            printf_s("t не может равняться 2 , т.к. деление на 0 недопустимо \n");
         }
         return 0;
+    }
+    if (z == 11) {
+        printf_s("z не может равняться 4, т.к. деление на 0 недопустимо \n");
+        return 0;
+    }
+    if (t == -2 || t == 2) {
+        if (t == -2) {
+            printf_s("t не может равняться -2 , т.к. деление на 0 недопустимо \n");
+        }
+        else {
+            printf_s("t не может равняться 2 , т.к. деление на 0 недопустимо \n");
+        }
     }
     ans = alg(x, y, z, t);
     printf("%lf", ans);
